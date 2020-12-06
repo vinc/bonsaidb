b8fa12e669dca9e38106671d8898665dd8b7fe00
Vincent Ollivier <v@vinc.cc>
2020-12-06 16:04:48 +0100

    Add list helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def split_list(list)
+    list.split(/, ?/)
+  end
+
+  def humanize_list(list)
+    res = ''
+    array = split_list(list)
+    array.each_with_index do |item, i|
+      if i == 0
+        res += item
+      elsif i == array.size - 1
+        if i == 1
+          res += " or #{item}"
+        else
+          res += ", or #{item}"
+        end
+      else
+        res += ", #{item}"
+      end
+    end
+    res
+  end
 end
