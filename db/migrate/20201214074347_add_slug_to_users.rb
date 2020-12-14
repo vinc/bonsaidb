4f03cb9a95dc479d429e1d9a3fd11139f192bef8
Vincent Ollivier <v@vinc.cc>
2020-12-14 09:05:33 +0100

    Trigger slug creation on db migrate

--- db/migrate/20201214074347_add_slug_to_users.rb
+++ db/migrate/20201214074347_add_slug_to_users.rb
@@ -2,5 +2,6 @@ class AddSlugToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :slug, :string
     add_index :users, :slug, unique: true
+    User.find_each(&:save)
   end
 end
